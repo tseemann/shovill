a92815ea978d909fb3d9625510f12b886a68a9ff
Torsten Seemann <torsten.seemann@gmail.com>
2018-05-06 10:29:14 +1000

    Run tests outside travis

---
+++ test/test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eu
+
+TESTDIR=$(dirname $0)
+SHOVILL="$TESTDIR/../bin/shovill"
+OUTDIR="$TESTDIR/testout"
+
+echo "TESTDIR = $TESTDIR"
+echo "OUTDIR = $OUTDIR"
+echo "SHOVILL = $SHOVILL"
+
+$SHOVILL --version
+$SHOVILL --check
+$SHOVILL --help
+! $SHOVILL --doesnotexist
+$SHOVILL --outdir "$OUTDIR" --R1 "$TESTDIR/R1.fq.gz" --R2 "$TESTDIR/R2.fq.gz" \
+	--ram 3 --cpus 0 --depth 25 --force
+grep '>' "$OUTDIR/contigs.fa"
+rm -frv "$OUTDIR"
