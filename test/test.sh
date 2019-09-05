1b3e017309167030ed7a5c6431db13bd4b4a4810
Torsten Seemann <torsten.seemann@gmail.com>
2019-09-05 10:43:06 +1000

    Better defaults / auto cpu ram

--- test/test.sh
+++ test/test.sh
@@ -14,7 +14,7 @@ $SHOVILL --version
 $SHOVILL --check
 $SHOVILL --help
 ! $SHOVILL --doesnotexist
-$SHOVILL --outdir "$OUTDIR" --R1 "$TESTDIR/R1.fq.gz" --R2 "$TESTDIR/R2.fq.gz" \
-	--ram 3 --cpus 8 --depth 25 --force
-grep '>' "$OUTDIR/contigs.fa"
 rm -frv "$OUTDIR"
+$SHOVILL --outdir "$OUTDIR" --R1 "$TESTDIR/R1.fq.gz" --R2 "$TESTDIR/R2.fq.gz" \
+	 --depth 25 --force --trim
+grep '>' "$OUTDIR/contigs.fa" | head
